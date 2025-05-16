878b2e9c2f556822bf084c39c315c1f5153fa305
Zeno Hug <zhug@eiger-ln003.cscs.ch>
2025-05-16 16:03:23 +0200

    restructured
    Former-commit-id: 632ef30bcfa1504168c79573af6ad5318c575709

--- old_plotting/plot-2.sh
+++ old_plotting/plot-2.sh
@@ -3,7 +3,7 @@
 cp wow.py ../simulation_outer_domain/
 module load cray/23.12
 module load cray-python/3.11.5
-source venv-plot/bin/activate
+source ../runscripts/venv-plot/bin/activate
 cd ../simulation_outer_domain/ 
 
 python wow.py
--- old_plotting/plot-flights-diff.sh
+++ old_plotting/plot-flights-diff.sh
@@ -3,7 +3,7 @@
 cp plot_flights_diff.py ../simulation_outer_domain/
 module load cray/23.12
 module load cray-python/3.11.5
-source venv-plot/bin/activate
+source ../runscripts/venv-plot/bin/activate
 cd ../simulation_outer_domain/ 
 
 python plot_flights_diff.py
