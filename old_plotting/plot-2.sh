351e6debe627e4756759aa3cf28ddaa4c800b837
Zeno Hug <zhug@eiger-ln004.cscs.ch>
2025-08-13 13:01:37 +0200

    more renaming
    Former-commit-id: 669502f58151a27a10d5bcc4b52cee176d2a8149

--- old_plotting/plot-2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp wow.py ../simulation_outer_domain/
-module load cray/23.12
-module load cray-python/3.11.5
-source ../runscripts/venv-plot/bin/activate
-cd ../simulation_outer_domain/ 
-
-python wow.py
